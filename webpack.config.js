e54b9505cbc0252a2541ec9cba60f1a5c41e0c7c
donsartain <donsartain@gamestop.com>
2018-12-14 14:58:46 -0600

    Many attempts to get webpack to run the postCSS to make tailwind.css work.

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,49 @@
-var webpack = require('webpack');
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+
 module.exports = {
-    context: __dirname + '/app',
-    entry: {
-        app: './app.js',
-        vendor: ['angular']
-    },
-    output: {
-        path: __dirname + '/js',
-        filename: 'app.bundle.js'
-    },
-    plugins: [
-        new webpack.optimize.CommonsChunkPlugin(/* chunkName= */"vendor", /* filename= */"vendor.bundle.js")
+  mode: "development",
+  entry: {
+    app: './src/main.ts',
+    vendor: ['./angular']
+  },
+  output: {
+    path: path.join(__dirname, 'src/js'),
+    filename: '[name].min.js'
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.json', '.css']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader"
+      },
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader"
+        ],
+      },
     ]
-};
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "src/styles.css",
+      chunkFilename: "src/css/main.css"
+    })
+  ]
+}
